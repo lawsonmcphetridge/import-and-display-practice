21b5f1939e0c29c42426a0057239770f0b1d45c3
Lawson <lawsonmcphetridge2020@gmail.com>
2022-07-29 15:51:54 -0700

    made 3rd function and array

---
+++ cars-array-three.js
@@ -0,0 +1,10 @@
+export const cars = [
+    {
+        brand: 'ram',
+        make: {
+            hauling: 2500,
+            fuel: 'turbo diesel',
+              },
+        year: 2020,
+    },
+];

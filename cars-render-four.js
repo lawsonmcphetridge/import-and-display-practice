f3aba7117eb3f012262bfb42869055464efa1b8e
Lawson <lawsonmcphetridge2020@gmail.com>
2022-08-04 14:44:56 -0700

    changed naming

--- cars-render-four.js
+++ cars-render-four.js
@@ -2,20 +2,23 @@ export function renderSecondCar(car) {
     const secondCarContainerEl = document.createElement('div');
     const makeEl = document.createElement('h1');
     const modleEl = document.createElement('p');
-    const colorRedEl = document.createElement('p');
-    const colorWhiteEl = document.createElement('p');
-    const colorBlueEl = document.createElement('p');
+    const colorEl = document.createElement('p');
     const yearEl = document.createElement('p');
     makeEl.textContent = car.make;
     modleEl.textContent = car.modle;
-    colorRedEl.textContent = car.color[0];
-    colorWhiteEl.textContent = car.color[1];
-    colorBlueEl.textContent = car.color[2];
+
+
+    for (let color of car.color) {
+        const carEl = document.createElement('div');
+        carEl.textContent = color;
+        colorEl.append(carEl);
+    }
+
     yearEl.textContent = car.year;
     secondCarContainerEl.classList.add('cars-four');
 
 
-    secondCarContainerEl.append(makeEl, modleEl, colorRedEl, colorWhiteEl, colorBlueEl, yearEl);
+    secondCarContainerEl.append(makeEl, modleEl, colorEl, yearEl);
 
     return secondCarContainerEl;
 }
